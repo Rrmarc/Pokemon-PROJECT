b6b02041c3355a0464dc307cd712033a4880e7a4
Rashell Marcelino <rmarcelino@nyc.yearup.org>
2018-11-13 16:54:38 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let pokeBallClick= document.getElementById('pokeball');
+
+let mainPokeballImg= document.getElementsByClassName('mainPokeball');
+
+pokeBallClick.onclick = function(){
+  mainPokeballImg.innerHTML= (opacity:1);
+
+
+}
