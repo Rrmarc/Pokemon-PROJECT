66954fa69048ac74fb243210d3e21eb746f0939f
Rashell Marcelino <rmarcelino@nyc.yearup.org>
2018-11-15 16:51:26 -0500

    day2 of pokeProject

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 let pokeBallClick= document.getElementById('pokeball');
+let mainPokeballImg= document.getElementsByClassName('mainPokeball')[0];
 
-let mainPokeballImg= document.getElementsByClassName('mainPokeball');
-
-pokeBallClick.onclick = function(){
-  mainPokeballImg.innerHTML= (opacity:1);
-
-
+function firstChange(){
+   if(mainPokeballImg.style.display=== 'none'){
+     mainPokeballImg.style.display='block';
+     console.log('show');
+   } else{
+     mainPokeballImg.style.display = 'none';
+     console.log('hidde');
+   }
 }
+pokeBallClick.addEventListener('click', firstChange );
+// end of first click event
+
+// conecting API
+
+axios.get('http://fizal.me/pokeapi/api/v2/name/pikachu.json')
+
+  .then(function (response){
+
+console.log(response.data);
+})
+  // .then(newfunc);
+  // function firstPokeFucntion(response) {
+  //   // handle success
+  //   console.log(response.data);
+  // }
